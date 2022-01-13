041122c0e3e2f8775858c81456de40475b75a315
Megan Campbell <megancampbell201@gmail.com>
2022-01-13 14:58:39 -0500

    prompt question for password length

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,10 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+  
+  
+function generatePassword() {
+  var passwordLength = window.prompt("How many characters would you like your password to contain? Please choose a length between 8 - 128.");
+
+}
